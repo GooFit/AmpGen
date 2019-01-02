a72eda564cdc2212c90f65f00bc943281b03bf53
tevans <timothy.david.evans@cern.ch>
2019-01-02 10:25:02 +0100

    Fixes for compiling on MACOSX

--- AmpGen/ProfileClock.h
+++ AmpGen/ProfileClock.h
@@ -2,8 +2,8 @@
 
 namespace AmpGen{
   struct ProfileClock {
-    std::chrono::system_clock::time_point t_start; 
-    std::chrono::system_clock::time_point t_end; 
+    std::chrono::time_point<std::chrono::high_resolution_clock> t_start; 
+    std::chrono::time_point<std::chrono::high_resolution_clock> t_end; 
     ProfileClock() : t_start(   std::chrono::high_resolution_clock::now()) {}
     void stop(){ t_end = std::chrono::high_resolution_clock::now() ; }
     operator double() const { return std::chrono::duration<double, std::milli>( t_end - t_start ).count() ; } ; 
