4c21a984bc3481ed9b57d6cc5259625990bb8dea
Henry Schreiner <henryschreineriii@utexas.edu>
2019-02-01 10:36:59 +0000

    Henryiii fixmac

--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -300,7 +300,7 @@ bool file_exists( const std::string& name )
 
 std::string rtrim( std::string s )
 {
-  s.erase( std::find_if( s.rbegin(), s.rend(), std::not1( std::ptr_fun<int, int>( std::isspace ) ) ).base(), s.end() );
+  s.erase( std::find_if( s.rbegin(), s.rend(), [](unsigned char c){ return !std::isspace(c); }).base(), s.end() );
   return s;
 }
 
@@ -309,7 +309,7 @@ std::string trim( std::string s ) { return ltrim( rtrim( s ) ); }
 
 std::string ltrim( std::string s )
 {
-  s.erase( s.begin(), std::find_if( s.begin(), s.end(), std::not1( std::ptr_fun<int, int>( std::isspace ) ) ) );
+  s.erase( s.begin(), std::find_if( s.begin(), s.end(), [](unsigned char c){ return !std::isspace(c); }));
   return s;
 }
 
