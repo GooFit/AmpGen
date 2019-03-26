01281fed3af2b9bfd01efdaf51b5f23fc5682550
tevans1260 <tevans1260@users.noreply.github.com>
2019-03-26 20:27:04 +0100

    Update build_docs.sh

--- .ci/build_docs.sh
+++ .ci/build_docs.sh
@@ -71,7 +71,9 @@ echo "" > .nojekyll
 ##### Generate the Doxygen code documentation and log the output.          #####
 echo 'Generating Doxygen code documentation...'
 # Redirect both stderr and stdout to the log file AND the console.
-cp -r $(dirname $DOXYFILE/*) .
+cp -r $(dirname $DOXYFILE)/* .
+pwd 
+ls 
 
 doxygen $DOXYFILE 2>&1 | tee doxygen.log
 
