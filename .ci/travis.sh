339bd1c086f656f3053506513d0b20fdecc3a046
tevans1260 <timothy.david.evans@cern.ch>
2019-03-27 15:02:41 +0100

    Add generator test to travis.sh

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -8,10 +8,16 @@ cmake .. -DUSE_OPENMP=0
 cmake --build . -- -j2
 
 set +evx
-echo -e 'travis_fold:end:script.build\\r'
-echo -en 'travis_fold:start:script.test\\r'
-echo "Testing..."
-set -evx
 
-ctest --output-on-failure
+cd ..
+
+./build/bin/Generator options/example_b2kstarll.opt --nEvents 1000
+
+
+# echo -e 'travis_fold:end:script.build\\r'
+# echo -en 'travis_fold:start:script.test\\r'
+# echo "Testing..."
+# set -evx
+
+# ctest --output-on-failure
 
