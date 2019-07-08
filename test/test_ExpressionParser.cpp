acc331a2ac19edaf5c27cef17d7a35c02158617f
tevans1260 <tevans1260@gmail.com>
2019-07-08 23:42:00 +0200

    Fix unit tests under boost 1.70.0

--- test/test_ExpressionParser.cpp
+++ test/test_ExpressionParser.cpp
@@ -9,8 +9,19 @@ namespace utf = boost::unit_test;
 #include "AmpGen/MinuitParameterSet.h"
 
 using namespace AmpGen; 
+  
+BOOST_AUTO_TEST_CASE( simple_numericalExpressions ){
+  auto test = [](const std::string& expr) -> double { return std::real( ExpressionParser::parse(expr)() ); }; 
+  BOOST_CHECK( test("1 + 2 * 3"                             ) ==  1 + 2 * 3 );
+  BOOST_CHECK( test("4 + 2                          "       ) ==  6 ); 
+  BOOST_CHECK( test("3 * 6 - 7 + 2                  "       ) ==  3 * 6 - 7 + 2 );
+  BOOST_CHECK( test("6 * 2 + ( 5 - 3 ) * 3 - 8        "     ) ==  6 * 2 + ( 5 - 3)*3 -8 );
+  BOOST_CHECK( test("( 3 + 4 ) + 7 * 2 - 1 - 9        "     ) ==  3 + 4 + 7 * 2 - 1 - 9);
+  BOOST_CHECK( test("5 - 2 + 4 * ( 8 - ( 5 + 1 ) ) + 9  "   ) ==  5 - 2 + 4 *( 8 - (5+1)) +9 );
+  BOOST_CHECK( test("( 8 - 1 + 3 ) * 6 - ( ( 3 + 7 ) * 2 )" ) == (8-1+3)*6 - ((3+7)*2) );
+}
 
-BOOST_AUTO_TEST_CASE( test_ExpressionParser ) {
+BOOST_AUTO_TEST_CASE( parametericExpressions ) {
 
   double a = 1.0;
   double b = 0.5; 
@@ -18,13 +29,12 @@ BOOST_AUTO_TEST_CASE( test_ExpressionParser ) {
   double d = 0.2;
   double f = 3.13;
   
-
   MinuitParameterSet mps( {
-      new MinuitParameter("a",MinuitParameter::Flag::Float, a,0. )
-  ,   new MinuitParameter("b",MinuitParameter::Flag::Float, b,0. )
-  ,   new MinuitParameter("c",MinuitParameter::Flag::Float, c, 0. )
-  ,   new MinuitParameter("d",MinuitParameter::Flag::Float, d, 0. )
-  ,   new MinuitParameter("f",MinuitParameter::Flag::Float, f, 0. )
+    new MinuitParameter("a", MinuitParameter::Flag::Float, a, 0.)
+  , new MinuitParameter("b", MinuitParameter::Flag::Float, b, 0.)
+  , new MinuitParameter("c", MinuitParameter::Flag::Float, c, 0.)
+  , new MinuitParameter("d", MinuitParameter::Flag::Float, d, 0.)
+  , new MinuitParameter("f", MinuitParameter::Flag::Float, f, 0.)
   });
 
   double pi = M_PI;
@@ -57,14 +67,8 @@ BOOST_AUTO_TEST_CASE( test_ExpressionParser ) {
   BOOST_CHECK( test("(b+a/b) * (a-b/a)")                           == (b+a/b) * (a-b/a) );
   BOOST_CHECK( test("(0.1*a+1)*a+1.1-sin(a)-log(a)/a*3/4")         == (0.1*a+1)*a+1.1-sin(a)-log(a)/a*3/4 );
   BOOST_CHECK( test("sin(2 * a) + cos(pi / b)")                    == sin(2 * a) + cos(pi / b) );
-  BOOST_CHECK( test("a - ( b + c ) "                        ) ==  a - ( b + c ) );
-  BOOST_CHECK( test("a - b + c - d / b + f - a "            ) ==  a - b + c - d / b + f -a  );
-  BOOST_CHECK( test("1 + 2 * 3"                             ) ==  1 + 2 * 3 );
-  BOOST_CHECK( test("4 + 2                          "       ) ==  6 ); 
-  BOOST_CHECK( test("3 * 6 - 7 + 2                  "       ) ==  3 * 6 - 7 + 2 );
-  BOOST_CHECK( test("6 * 2 + ( 5 - 3 ) * 3 - 8        "     ) ==  6 * 2 + ( 5 - 3)*3 -8 );
-  BOOST_CHECK( test("( 3 + 4 ) + 7 * 2 - 1 - 9        "     ) ==  3 + 4 + 7 * 2 - 1 - 9);
-  BOOST_CHECK( test("5 - 2 + 4 * ( 8 - ( 5 + 1 ) ) + 9  "   ) ==   5 - 2 + 4 *( 8 - (5+1)) +9 );
-  BOOST_CHECK( test("( 8 - 1 + 3 ) * 6 - ( ( 3 + 7 ) * 2 )" ) == (8-1+3)*6 - ((3+7)*2) );
-
+  BOOST_CHECK( test("a - ( b + c ) "                        )      ==  a - ( b + c ) );
+  BOOST_CHECK( test("a - b + c - d / b + f - a "            )      ==  a - b + c - d / b + f -a  );
 }
+
+
