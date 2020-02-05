1ee34c32bc6702e1f49aa07077e8bbf1ab545422
tevans1260 <timothy.david.evans@cern.ch>
2020-02-05 16:24:53 +0100

    Try to use conda install of cxx

--- .ci/build_root_linux.sh
+++ .ci/build_root_linux.sh
@@ -5,23 +5,15 @@ set -evx
 
 pushd $DEPS_DIR 
 
-# root_v6.12.06.Linux-ubuntu16-x86_64-gcc5.4.tar.gz
-# ROOT_URL="https://root.cern.ch/download/root_v6.12.06.Linux-ubuntu16-x86_64-gcc5.4.tar.gz"
-# ROOT_URL="https://root.cern.ch/download/root_v6.13.08.Linux-fedora28-x86_64-gcc8.0.tar.gz"
-# 
-# if [[ ! -f "${DEPS_DIR}/root/bin/root-config" ]] ; then
-#   echo "Downloading Root"
-#   mkdir -p root
-#   travis_retry wget --no-check-certificate --quiet -O - "${ROOT_URL}" | tar --strip-components=1 -xz -C root
-# fi
 wget -nv http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
+bash miniconda.sh -b -p $DEPS_DIR/miniconda
+export PATH="$DEPS_DIR/miniconda/bin:$PATH"
 hash -r
 conda config --add channels conda-forge
 conda install --quiet --yes -c conda-forge/label/gcc8 root 
 
-source "$HOME/miniconda/bin/thisroot.sh"
+source "$DEPS_DIR/miniconda/bin/thisroot.sh"
+export CXX="$DEPS_DIR/miniconda/bin/g++"
 popd
 
 set +evx
