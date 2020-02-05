2a1801f0260430eb4808b9e0236d0ff0063d4325
tevans1260 <timothy.david.evans@cern.ch>
2020-02-05 10:42:57 +0100

    add actual build root options for osx

--- .ci/build_root_osx.sh
+++ .ci/build_root_osx.sh
@@ -1,1 +1,15 @@
-echo "Building ROOT for OSX..."
+echo "Building ROOT for OSX [DIR=${DEPS_DIR}]"
+
+pushd $DEPS_DIR 
+
+# root_v6.12.06.Linux-ubuntu16-x86_64-gcc5.4.tar.gz
+ROOT_URL="https://root.cern.ch/download/root_v6.12.06.macosx64-10.12-clang90.tar.gz"
+
+if [ ! -f "${DEPS_DIR}/root/bin/root-config" ] ; then
+  echo "Downloading Root"
+  mkdir -p root
+  travis_retry wget --no-check-certificate --quiet -O - "${ROOT_URL}" | tar --strip-components=1 -xz -C root
+fi
+
+source "${DEPS_DIR}/root/bin/thisroot.sh"
+popd
