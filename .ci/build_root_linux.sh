fc29d33f2842f7f9f08412473cbd3e7ac4b73b08
tevans1260 <timothy.david.evans@cern.ch>
2020-02-05 15:04:54 +0100

    try to use conda for ROOT

--- .ci/build_root_linux.sh
+++ .ci/build_root_linux.sh
@@ -7,13 +7,20 @@ pushd $DEPS_DIR
 
 # root_v6.12.06.Linux-ubuntu16-x86_64-gcc5.4.tar.gz
 # ROOT_URL="https://root.cern.ch/download/root_v6.12.06.Linux-ubuntu16-x86_64-gcc5.4.tar.gz"
-ROOT_URL="https://root.cern.ch/download/root_v6.13.08.Linux-fedora28-x86_64-gcc8.0.tar.gz"
+# ROOT_URL="https://root.cern.ch/download/root_v6.13.08.Linux-fedora28-x86_64-gcc8.0.tar.gz"
+# 
+# if [[ ! -f "${DEPS_DIR}/root/bin/root-config" ]] ; then
+#   echo "Downloading Root"
+#   mkdir -p root
+#   travis_retry wget --no-check-certificate --quiet -O - "${ROOT_URL}" | tar --strip-components=1 -xz -C root
+# fi
+wget -nv http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+hash -r
+conda config --add channels conda-forge
+conda install -c conda-forge/label/gcc8 root 
 
-if [[ ! -f "${DEPS_DIR}/root/bin/root-config" ]] ; then
-  echo "Downloading Root"
-  mkdir -p root
-  travis_retry wget --no-check-certificate --quiet -O - "${ROOT_URL}" | tar --strip-components=1 -xz -C root
-fi
 
 source "${DEPS_DIR}/root/bin/thisroot.sh"
 popd
