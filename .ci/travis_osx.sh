a6c27510e08b9460c1cb7557bded9c81b0919c17
tevans1260 <timothy.david.evans@cern.ch>
2020-02-05 10:33:27 +0100

    split travis job for osx/linus

---
+++ .ci/travis_osx.sh
@@ -0,0 +1,21 @@
+echo -en 'travis_fold:start:script.build\\r'
+echo "Building..."
+echo "Building under OS: $TRAVIS_OS_NAME"
+mkdir -p build
+cd build
+echo "CMake-ing"
+cmake .. -DUSE_OPENMP=0
+echo "Building ..."
+cmake --build . -- -j2
+cd ..
+echo "Running test job ..."
+./build/bin/Generator options/example_b2kstarll.opt --CompilerWrapper::Verbose --nEvents 1000
+
+
+# echo -e 'travis_fold:end:script.build\\r'
+# echo -en 'travis_fold:start:script.test\\r'
+# echo "Testing..."
+# set -evx
+
+# ctest --output-on-failure
+
