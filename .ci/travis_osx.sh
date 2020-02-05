8a1ed0d6d9348cdfea90ec08e48dc4e20a7f4b5b
tevans1260 <timothy.david.evans@cern.ch>
2020-02-05 14:10:57 +0100

    still fixing osx ci

--- .ci/travis_osx.sh
+++ .ci/travis_osx.sh
@@ -4,9 +4,9 @@ echo "Building under OS: $TRAVIS_OS_NAME"
 mkdir -p build
 cd build
 echo "CMake-ing"
-cmake .. -DUSE_OPENMP=0
+cmake ..  -DUSE_OPENMP=0 -DCMAKE_CXX_COMPILER=clang
 echo "Building ..."
-cmake --build . -- -j2
+cmake --build . -- -j2 
 cd ..
 echo "Running test job ..."
 ./build/bin/Generator options/example_b2kstarll.opt --CompilerWrapper::Verbose --nEvents 1000
