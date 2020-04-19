961dbe403ac43469df8527ecaca35bd7635ad388
tevans1260 <timothy.david.evans@cern.ch>
2020-04-19 15:43:45 +0200

    fix travis...

--- .ci/build_root_linux.sh
+++ .ci/build_root_linux.sh
@@ -7,7 +7,7 @@ hash -r
 conda config --add channels conda-forge
 conda config --set channel_priority strict
 # conda install --quiet --yes -c conda-forge/label/gcc8 root zstd 
-conda create -n my_root_env root -c conda-forge/label/gcc8
+conda create --quiet -n my_root_env root -c conda-forge/label/gcc8
 
 # source "$DEPS_DIR/miniconda/bin/thisroot.sh"
 # export CXX="$DEPS_DIR/miniconda/bin/g++"
