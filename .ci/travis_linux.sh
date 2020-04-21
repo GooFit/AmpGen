3f193e8eb90ca1b47922696750eeff740d0db8ee
tevans1260 <timothy.david.evans@cern.ch>
2020-04-21 13:51:42 +0200

    increase verbosity of ci

--- .ci/travis_linux.sh
+++ .ci/travis_linux.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -evx
 # from https://stackoverflow.com/questions/55342122/conda-activate-on-travis-ci
 export PATH="$DEPS_DIR/miniconda/bin:$PATH"
 . $(conda info --root)/etc/profile.d/conda.sh
@@ -8,7 +9,6 @@ conda activate env_${TRAVIS_OS_NAME}
 echo -en 'travis_fold:start:script.build\\r'
 echo "Building..."
 echo "Building under OS: $TRAVIS_OS_NAME, CXX =$CXX"
-set -evx
 
 mkdir -p build.conda
 cd build.conda
