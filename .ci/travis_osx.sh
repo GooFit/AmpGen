d39508a1a2cf43f08e76192a5f9fbada8dc74967
tevans1260 <tevans1260@gmail.com>
2020-08-05 21:32:27 +0200

    disable openmp  in the travis job

--- .ci/travis_osx.sh
+++ .ci/travis_osx.sh
@@ -10,16 +10,9 @@ echo "Building under OS: $TRAVIS_OS_NAME"
 mkdir -p build
 cd build
 echo "CMake-ing, CXX = $CXX"
-cmake .. -DCMAKE_CXX_COMPILER=clang -DUSE_SIMD=""
+cmake .. -DCMAKE_CXX_COMPILER=clang -DUSE_SIMD="" -DUSE_OPENMP=0
 echo "Building ..."
 cmake --build . -- -j2 
 cd ..
 echo "Running test job ..."
 ./build/bin/Generator options/example_b2kstarll.opt --CompilerWrapper::Verbose --nEvents 10000
-# echo -e 'travis_fold:end:script.build\\r'
-# echo -en 'travis_fold:start:script.test\\r'
-# echo "Testing..."
-# set -evx
-
-# ctest --output-on-failure
-
