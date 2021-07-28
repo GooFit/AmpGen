f97c5644e03b0b5ba4d6305861ded5648bf4395e
Timothy David Evans <timothy.david.evans@cern.ch>
2021-07-28 14:40:10 +0200

    fix missing include in Pade.cpp

--- src/Pade.cpp
+++ src/Pade.cpp
@@ -1,7 +1,9 @@
-#include "AmpGen/Pade.h"
+#include <cmath>
 
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_linalg.h>
+
+#include "AmpGen/Pade.h"
 
 std::vector<double> AmpGen::detail::solve_pade(const std::function<double(const double&)>& fcn,
     const double& min,
@@ -17,12 +19,12 @@ std::vector<double> AmpGen::detail::solve_pade(const std::function<double(const 
   std::vector<double> rest( 2*N+1, 0);
   if( strat < 4 ){
     for(unsigned eq = 0 ; eq < 2*N+1; ++eq) 
-      samples[eq] = pow( eq/double(2*N), strat + 1);
+      samples[eq] = std::pow( eq/double(2*N), strat + 1);
   }
   for( unsigned eq = 0 ; eq < 2*N+1; ++eq ){
     rest[eq] = fcn( samples[eq]*(max-min) + min);
-    for(unsigned i = 0; i <= N; ++i) gsl_matrix_set(solver, eq, i, pow(samples[eq],i) );
-    for(unsigned i = 1; i <= N; ++i) gsl_matrix_set(solver, eq, i+N, -rest[eq]* pow(samples[eq],i) );
+    for(unsigned i = 0; i <= N; ++i) gsl_matrix_set(solver, eq, i, std::pow(samples[eq],i) );
+    for(unsigned i = 1; i <= N; ++i) gsl_matrix_set(solver, eq, i+N, -rest[eq]* std::pow(samples[eq],i) );
   }
   gsl_permutation *p = gsl_permutation_alloc(2*N+1);
   int s;
