b1632af91c3ba7ea961b64218aebdd594ac8fff1
tevans <timothy.david.evans@cern.ch>
2023-10-03 10:13:07 +0200

    add Minimiser::Precision flag and tighten default

--- src/Minimiser.cpp
+++ src/Minimiser.cpp
@@ -120,6 +120,7 @@ void Minimiser::prepare()
   std::string algorithm = NamedParameter<std::string>( "Minimiser::Algorithm", "Migrad");
   size_t maxCalls       = NamedParameter<size_t>( "Minimiser::MaxCalls"  , 100000);
   double tolerance      = NamedParameter<double>( "Minimiser::Tolerance" , 1.0);
+  double precision      = NamedParameter<double>( "Minimiser::Precision" , 1e-15);
   m_printLevel          = NamedParameter<PrintLevel>( "Minimiser::PrintLevel", PrintLevel::Info); 
   unsigned printLevelMinuit2 = NamedParameter<unsigned>("Minimiser::Minuit2MinimizerPrintLevel", m_printLevel == PrintLevel::VeryVerbose ? 3 : 0 );
   if( m_printLevel == PrintLevel::Invalid )
@@ -136,7 +137,7 @@ void Minimiser::prepare()
   m_minimiser->SetMaxFunctionCalls( maxCalls );
   m_minimiser->SetMaxIterations( 100000 );
   m_minimiser->SetTolerance( tolerance );
-  m_minimiser->SetPrecision( 1e-10 );
+  m_minimiser->SetPrecision( precision );
   m_minimiser->SetStrategy( 2 );
   m_minimiser->SetPrintLevel( printLevelMinuit2 ); // turn off minuit printing 
   m_mapping.clear();
